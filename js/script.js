7b280370a9f6634ca93d4731c033ea6bcfe4c61f
Marika <m.abashidze@ssu.edu.ge>
2021-07-26 20:27:20 +0400

    lesson-16

---
+++ js/script.js
@@ -0,0 +1,99 @@
+// I
+// let cars = ['audi', 'bmw', 'lexus', 'volkswagen', 'porsche', 'audi'];
+
+// for (let x = 1; x < cars.length; x++) {
+//     console.log(cars[x]);
+// }
+
+
+
+
+//II
+// let names = ['megi','gulo','oto','mari','ana','lasha'];
+// for (let i = 0; i < names.length; i++ ){
+//     if (names [i].length > 3){
+//         console.log(names [i]);
+//     }
+// }
+
+
+
+
+// III
+// let numbers = [122,16,17,20,7,33,25,26,8];
+// for (let i=0; i < numbers.length; i++) {
+//     if(numbers[i] % 2 ==0) {
+//         console.log(numbers [i]);
+//     }
+    
+// }
+
+// let numbers = [122,16,17,20,7,33,25,26,8];
+// for (let i=0; i < numbers.length; i++) {
+//     if(numbers[i] % 2 == 1) {
+//         console.log(numbers [i]);
+//     }
+// }
+
+
+
+
+
+// IV
+
+// let numbers= [15, 67, 100, 5, 7, [80, 500] ]; 
+
+// console.log(numbers [5][1]);
+
+
+
+
+
+// V
+// var person = {
+//     firstname: "john",
+//     lastname: "doe",
+//     age: 50,
+//     eyecolor: "blue"
+//   };
+  
+//   console.log(person.eyecolor);
+
+
+
+
+// VI
+// var numbers = [10,44,'megi', 'maia'];
+// for (var item of numbers){
+//     console.log(item);
+// }
+
+
+
+
+// VII
+
+// var names = ['sandro','oto','mari','ana','nika'];
+// if (names.length == 5 && names [names.length - 1]== 'nika'){
+//     console.log("მასივის რაოდენობა: 5 და ბოლო ელემენტია ნიკა");
+// }
+
+// if (names.length == 7 && names [0] == 'sandro'){
+//     console.log("მასივის რაოდენობა: 7 და პირველი ელემენტია სანდრო");
+// }
+
+
+
+// VII
+//6 === “6”
+// დაუბრუნებს FALSE-ს,რადგან სამი ტოლობით შედარების დროს ცვლადების ტიპების კონვერტაცია არ ხდება.
+
+
+  
+
+
+
+
+
+
+
